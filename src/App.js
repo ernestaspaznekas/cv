cc38eb3adcd1770b64ca6331f1b1af24a4502e66
Ernestas Paznekas <ernestas.paznekas@gmail.com>
2017-10-11 17:57:37 +0300

    Creds

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to My resume build with React</h1>
+          <h1 className="App-title">Welcome to Ernestas Paznekas resume build with React</h1>
         </header>
         <div>
           <p>
